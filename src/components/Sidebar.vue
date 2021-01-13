99ba2b975de07196826f8c33e13262f597b85247
JordanInsights <jordan@theinsightspeople.com>
2021-01-13 13:42:28 +0000

    Add packages and start of sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sidebar_container">
+    <div class="buttons_container">
+      <font-awesome-icon icon="user-secret" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .sidebar_container {
+    width: 5vw;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    top: 0;
+    background-color: cornflowerblue;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .buttons_container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import router from './router'
+
+library.add(faUserSecret)
 
 Vue.config.productionTip = false
 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.component(
+  'sidebar', require('./components/Sidebar.vue').default
+);
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
